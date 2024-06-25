f618916a59111ba897a1f6c8b2f4b7ad9c4c3e9a
fujisan <c0b2312277@edu.teu.ac.jp>
2024-06-25 14:41:59 +0900

    よろこび

--- ex3/fight_kokaton.py
+++ ex3/fight_kokaton.py
@@ -169,9 +169,10 @@ def main():
 
         if bomb is not None:
             if beam is not None:
-                if bomb.rct.colliderect(beam.rct):
+                if bomb.rct.colliderect(beam.rct): # ビームと爆弾のぶつかり判定
                     bomb=None
                     beam=None
+                    bird.change_img(6,screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
